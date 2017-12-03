08f799206e061020971046800ccbc5dd8a7e1825
Kristy <kristy@kristylaporte.com>
2017-12-03 09:25:33 -0500

    Adds input and instructions for day three, part one.

---
+++ 03/03.js
@@ -0,0 +1,44 @@
+/*
+
+ADVENT OF CODE 2017
+
+===========================
+DAY X: 
+===========================
+
+Instructions go here.
+
+*/
+
+// INPUT FOR THIS DAY'S PUZZLE
+// Using node.js's filesync module to get the input from external file (syncronously):
+var fs = require('fs');
+var data;
+try {
+   data = fs.readFileSync(__dirname+'\\xx-input.txt', 'utf8');
+} catch (error) {
+    console.log('Error: ', error.stack)
+}
+
+
+/*
+
+PART ONE 
+---------
+
+Instructions go here.
+
+PART TWO
+------------
+
+Instructions go here.
+
+*/
+
+function solution(part) {
+    return "No solution yet..."
+}
+
+// OUTPUTTING OUR SOLUTION:
+console.log("Your solution for DAY X PART X should be... *drumroll*..." + solution(1));
+console.log("Your solution for DAY X PART X should be... *drumroll*..." + solution(2));
