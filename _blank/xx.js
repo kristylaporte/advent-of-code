79cd447d206428e1ca977724b28662c401274f69
Kristy <kristy@kristylaporte.com>
2017-12-05 19:26:01 -0500

    Adjusts solution function in template by adding sections for parts

--- _blank/xx.js
+++ _blank/xx.js
@@ -29,8 +29,26 @@ Instructions go here.
 */
 
 function solution(part) {
-    return "No solution yet..."
-}
+    
+        if (part == 1) {
+    
+            // FINDING SOLUTION TO PART ONE:
+    
+            // Return the solution:
+            return "No solution yet..." 
+    
+        } else if (part == 2) {
+    
+            // FINDING SOLUTION TO PART TWO:
+            
+            // Return the solution:
+            return "No solution yet..." 
+    
+        } else {
+            error('solution function must receive 1 or 2');
+        }
+    
+    }
 
 // OUTPUTTING OUR SOLUTION:
 console.log("Your solution for DAY X PART X should be... *drumroll*..." + solution(1));
