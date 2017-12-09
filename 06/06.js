c3b14a3502ce38d5cec049322e8e4b3a9686817a
Kristy <kristy@kristylaporte.com>
2017-12-09 11:16:51 -0500

    Adds files for day 6.

---
+++ 06/06.js
@@ -0,0 +1,55 @@
+/*
+
+ADVENT OF CODE 2017
+
+===========================
+DAY 6: 
+===========================
+
+Instructions go here.
+
+*/
+
+// INPUT FOR THIS DAY'S PUZZLE:
+var getInput = require(__dirname+'\\..\\'+'get-input.js');
+var data = getInput.getInput('06');
+
+/*
+
+PART ONE 
+---------
+
+Instructions go here.
+
+PART TWO
+------------
+
+Instructions go here.
+
+*/
+
+function solution(part) {
+    
+        if (part == 1) {
+    
+            // FINDING SOLUTION TO PART ONE:
+    
+            // Return the solution:
+            return "No solution yet..." 
+    
+        } else if (part == 2) {
+    
+            // FINDING SOLUTION TO PART TWO:
+            
+            // Return the solution:
+            return "No solution yet..." 
+    
+        } else {
+            error('solution function must receive 1 or 2');
+        }
+    
+    }
+
+// OUTPUTTING OUR SOLUTION:
+console.log("Your solution for DAY 6 PART 1 should be... *drumroll*..." + solution(1));
+console.log("Your solution for DAY 6 PART 2 should be... *drumroll*..." + solution(2));
