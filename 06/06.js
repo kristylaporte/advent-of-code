e473641135675b99a93b433f29b2a99b32591641
Kristy <kristy@kristylaporte.com>
2017-12-09 21:54:20 -0500

    Adds solution for Day 6 part 2

--- 06/06.js
+++ 06/06.js
@@ -39,9 +39,122 @@ Given the initial block counts in your puzzle input, how many redistribution cyc
 PART TWO
 ------------
 
-Instructions go here.
+Out of curiosity, the debugger would also like to know the size of the loop: starting from a state that has already been seen, how many block redistribution cycles must be performed before that same state is seen again?
+
+In the example above, 2 4 1 2 is seen again after four cycles, and so the answer in that example would be 4.
+
+How many cycles are in the infinite loop that arises from the configuration in your puzzle input?
 
 */
+
+
+
+
+
+
+ // FINDING SOLUTION TO PART ONE:
+
+ var resultCollection = new Set();
+ var banks = data.slice(); // using slice to make a copy of the data instead of reference
+ var cycleCount = 0;
+
+ resultCollection.add(banks.join());
+ var resultSeen = false;
+
+ // Do redistribution
+
+ while (resultSeen==false) {
+     
+     // Find highest number:
+     var highest = Math.max.apply(null, banks);
+     // Find first incidence of highest number:
+     var target = banks.indexOf(highest);
+     banks[target] = 0;
+     // Redistribution:
+     while (highest > 0 ) {
+         target++;
+         // If we go out of bounds with new index, index % banks.length will move us back to start of array
+         target = target % banks.length;
+         banks[target]++;
+         highest--;
+     }
+
+     cycleCount++;
+     
+     resultCollection.add(banks.join());
+     // Now see if this arrangement has been seen before:
+     if (cycleCount >= resultCollection.size ) {
+         resultSeen = true;
+     }
+
+ }
+
+ // Return the solution:
+ console.log('Part 1 is ' + cycleCount);
+
+ ///////////////////////////////// PART 2 //////////////////////////////////
+
+ var cycleCount2 = 0;
+ resultSeen = false;
+ var resultCollection2 = new Set();
+ resultCollection2.add(banks.join());
+
+ while (resultSeen==false) {
+    
+    // Find highest number:
+    var highest = Math.max.apply(null, banks);
+    // Find first incidence of highest number:
+    var target = banks.indexOf(highest);
+    banks[target] = 0;
+    // Redistribution:
+    while (highest > 0 ) {
+        target++;
+        // If we go out of bounds with new index, index % banks.length will move us back to start of array
+        target = target % banks.length;
+        banks[target]++;
+        highest--;
+    }
+
+    cycleCount2++;
+    
+    resultCollection2.add(banks.join());
+    // Now see if this arrangement has been seen before:
+    if (cycleCount2 >= resultCollection2.size ) {
+        resultSeen = true;
+    }
+
+}
+
+console.log('Part 2 is ' + cycleCount2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 function solution(part) {
     
@@ -51,7 +164,6 @@ function solution(part) {
 
             var resultCollection = new Set();
             var banks = data.slice(); // using slice to make a copy of the data instead of reference
-            console.log('banks when first set: '+banks);
             var cycleCount = 0;
 
             resultCollection.add(banks.join());
@@ -92,9 +204,73 @@ function solution(part) {
     
             // FINDING SOLUTION TO PART TWO:
             
+            var resultCollection = [];
+            
+            var banks = data.slice(); // using slice to make a copy of the data instead of reference
+            //console.log('banks when first set: '+banks);
+            var cycleCount = 0;
+
+            resultCollection.push(banks.join());
+            var resultSeen1 = false;
+            var resultSeen2 = false;
+            var resultToMatch;
+            var cyclesUntilSecondMatch = 0;
+
+            // Do redistribution
+
+            while (resultSeen2==false) {
+                
+                // Find highest number:
+                var highest = Math.max.apply(null, banks);
+                // Find first incidence of highest number:
+                var target = banks.indexOf(highest);
+                banks[target] = 0;
+                // Redistribution:
+                while (highest > 0 ) {
+                    target++;
+                    // If we go out of bounds with new index, index % banks.length will move us back to start of array
+                    target = target % banks.length;
+                    banks[target]++;
+                    highest--;
+                }
+
+                cycleCount++;
+                
+                resultCollection.push(banks.join());
+                //console.log(resultCollection); // array of strings like '1,12,54'...
+                // Now see if this arrangement has been seen before:
+                if (resultSeen1 == false) {
+                    // we haven't seen the first one yet so we need to check for that:
+                    
+                    //if (cycleCount >= resultCollection.length) { // fix this
+                    //    console.log('=== RESULT SEEN 1 happened, now watch for repeat ===')
+                    //    resultSeen1 = true;
+                    //    resultToMatch = resultCollection[resultCollection.length-1];
+                    //    console.log('resultToMatch: '+resultToMatch);
+                    //}
+                    for (i = 0; i < resultCollection.length; i++) {
+                        //console.log(banks.join())
+                        if (resultCollection[i] == banks.join()) {
+                            //console.log('=== RESULT SEEN 1 happened at cycleCount '+cycleCount+', now watch for repeat ===')
+                            resultSeen1 = true;
+                            resultToMatch = resultCollection[i];
+                            //console.log('resultToMatch: '+resultToMatch);
+                        }
+                    }
+                } else if (resultCollection[resultCollection.length-1] != resultToMatch ) {
+                    //console.log('resultToMatch: '+resultToMatch);
+                    //console.log('increase cyclesUntilSecondMatch ' + resultCollection[resultCollection.length-1]);
+                    cyclesUntilSecondMatch++;
+                } else if (resultCollection[resultCollection.length-1] == resultToMatch) {
+                    
+                    resultSeen2 == true;
+                }
+                
+            }
+
             // Return the solution:
-            return "No solution yet..." 
-    
+            return cyclesUntilSecondMatch;
+
         } else {
             error('solution function must receive 1 or 2');
         }
@@ -102,5 +278,6 @@ function solution(part) {
     }
 
 // OUTPUTTING OUR SOLUTION:
-console.log("Your solution for DAY 6 PART 1 should be... *drumroll*..." + solution(1));
-console.log("Your solution for DAY 6 PART 2 should be... *drumroll*..." + solution(2));
+//console.log("Your solution for DAY 6 PART 1 should be... *drumroll*..." + solution(1));
+//console.log("Your solution for DAY 6 PART 2 should be... *drumroll*..." + solution(2));
+*/
